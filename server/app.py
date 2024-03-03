7afff15a79c8fa313797523c80fcfc17a318c109
Anna Raptidou Cole <acrrj123@gmail.com>
2024-03-03 17:35:23 -0600

    Lab done

--- server/app.py
+++ server/app.py
@@ -16,12 +16,37 @@ db.init_app(app)
 
 api = Api(app)
 
-class Plants(Resource):
-    pass
+class Home(Resource):
+    def get(self):
+        response = {"message": "Welcome to the Plants RESTful API"}
+        return make_response(response, 200)
+    
+api.add_resource(Home, '/')
 
-class PlantByID(Resource):
-    pass
+class Plants(Resource):
+    def get(self):
+        plants_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(plants_dict_list, 200)
+    
+    def post(self):
+        new_plant = Plant(
+            name = request.get_json().get("name"),
+            image = request.get_json().get("image"),
+            price = request.get_json().get("price")
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(new_plant.to_dict(), 201)
         
+api.add_resource(Plants, '/plants')
+    
+class PlantByID(Resource):
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        return make_response(plant, 200)
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -21,5 +21,12 @@ with app.app_context():
         price=25.98,
     )
 
-    db.session.add_all([aloe, zz_plant])
+    orchid = Plant(
+        id=3,
+        name="Orchid",
+        image="https://cdn.shopify.com/s/files/1/0507/3754/5401/files/P2162_LOL_preset_ftd-mx-hero-sv-new.jpg?v=1689848923&width=768",
+        price=39.99,
+    )
+
+    db.session.add_all([aloe, zz_plant, orchid])
     db.session.commit()
